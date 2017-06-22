62093ffc135b64282242b502f996ba8d2a74fe29
Dann Bohn <djb44@Danns-MacBook-Pro.local>
2017-06-21 21:21:47 -0400

    add search funcs

--- random_gif/routes/index.py
+++ random_gif/routes/index.py
@@ -2,16 +2,17 @@ from random_gif import app
 from flask import session, render_template, request
 import giphypop
 
-@app.route('/')
+
+@app.route('/', methods=('GET', 'POST'))
 def random_gif():
     gif = giphypop.Giphy()
-    search = request.args.get('search', None)
+    if request.form:
+        search = request.form.get('search', None)
+    else:
+        search = None
     if search:
         random_gif = gif.random_gif(tag=search)
     else:
         random_gif = gif.random_gif()
     random_gif_url = random_gif.media_url
     return render_template('index.html', random_gif_url=random_gif_url, search=search)
-
-
-
---
+++ run.py
@@ -0,0 +1,3 @@
+from random_gif import app 
+
+app.run(debug=True)

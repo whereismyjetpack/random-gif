755c1df4a129bee585df32cff48faf6f5f533754
Dann Bohn <djb44@psu.edu>
2017-11-22 11:00:03 -0500

    remove giphy

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def random_gif():
-    gif = giphypop.Giphy()
-    search = request.args.get('search', None)
-    if search:
-        random_gif = gif.random_gif(tag=search)
-    else:
-        random_gif = gif.random_gif()    
-    random_gif_url = random_gif.media_url
-    return render_template('index.html', random_gif_url=random_gif_url, search=search)
+    #gif = giphypop.Giphy()
+    #search = request.args.get('search', None)
+    #if search:
+    #    random_gif = gif.random_gif(tag=search)
+    #else:
+    #    random_gif = gif.random_gif()    
+    #random_gif_url = random_gif.media_url
+    return render_template('index.html', random_gif_url=False, search=False)

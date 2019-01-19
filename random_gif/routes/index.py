95370dde6836ab3a8caeb5d18434b5dc4452ce95
Dann Bohn <dannbohn@gmail.com>
2019-01-19 12:58:54 -0500

    adds print statement

--- random_gif/routes/index.py
+++ random_gif/routes/index.py
@@ -17,5 +17,6 @@ def random_gif():
     #else:
     #    random_gif = gif.random_gif()
     #random_gif_url = random_gif.media_url
+    print('thing 1')
     return 'thing1'
     #return render_template('index.html', random_gif_url=random_gif_url, search=search, hostname=hostname, request=request)

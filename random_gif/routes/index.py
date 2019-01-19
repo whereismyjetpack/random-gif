aa65df5160d2568ef829189d1b73b9262eae8391
Dann Bohn <dannbohn@gmail.com>
2019-01-19 14:47:04 -0500

    this should fail

--- random_gif/routes/index.py
+++ random_gif/routes/index.py
@@ -18,5 +18,5 @@ def random_gif():
     #else:
     #    random_gif = gif.random_gif()
     #random_gif_url = random_gif.media_url
-    return 'thing2\n', random.choice(return_codes)
+    return 'thing1\n', random.choice(return_codes)
     #return render_template('index.html', random_gif_url=random_gif_url, search=search, hostname=hostname, request=request)

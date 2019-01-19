f62760164b0f09ed169abf4cec1106c4879d9526
Dann Bohn <dannbohn@gmail.com>
2019-01-19 13:21:59 -0500

    fix

--- random_gif/routes/index.py
+++ random_gif/routes/index.py
@@ -7,7 +7,6 @@ import socket
 @app.route('/', methods=('GET', 'POST'))
 def random_gif():
     #gif = giphypop.Giphy()
-    hostname = socket.gethostname()
     #if request.form:
     #    search = request.form.get('search', None)
     #else:
@@ -17,6 +16,6 @@ def random_gif():
     #else:
     #    random_gif = gif.random_gif()
     #random_gif_url = random_gif.media_url
-    print('thing 3')
-    return 'thing3'
+    print('thing 4')
+    return 'thing4'
     #return render_template('index.html', random_gif_url=random_gif_url, search=search, hostname=hostname, request=request)

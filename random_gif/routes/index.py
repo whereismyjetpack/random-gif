f071f782aab00e95bc11dc25ad7a5216849aa8cb
Dann Bohn <dannbohn@gmail.com>
2019-01-18 21:33:56 -0500

    fix

--- random_gif/routes/index.py
+++ random_gif/routes/index.py
@@ -16,6 +16,6 @@ def random_gif():
     #    random_gif = gif.random_gif(tag=search)
     #else:
     #    random_gif = gif.random_gif()
-    random_gif_url = random_gif.media_url
+    #random_gif_url = random_gif.media_url
     return 'Hello, world'
     #return render_template('index.html', random_gif_url=random_gif_url, search=search, hostname=hostname, request=request)
